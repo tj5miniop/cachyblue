4e084ae2f76133ddf2dd189f169292ccf4b93f53
tj5miniop <82961157+tj5miniop@users.noreply.github.com>
2025-02-16 22:03:43 +0000

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,6 +8,10 @@ dnf5 -y clean all
 
 echo 'Updated!' 
 
+#Reinstall ublue os repos
+dnf5 -y copr enable ublue/os-staging
+dnf5 -y copr enable ublue/os-akmods
+
 #Install CachyOS kernel 
 dnf5 -y copr enable bieszczaders/kernel-cachyos
 dnf5 -y update --refresh 
@@ -19,9 +23,11 @@ echo 'CachyOS Kernel Installed'
 #Add extra stuff for performance 
 dnf5 -y copr disable bieszczaders/kernel-cachyos
 dnf5 -y copr enable bieszczaders/kernel-cachyos-addons 
+dn5 -y update --refresh
+dnf5 -y install libcap-ng libcap-ng-devel procps-ng procps-ng-devel
 dnf5 -y install cachyos-settings cachyos-ksm-settings uksmd cachyos-ananicy-rules ananicy-cpp --allowerasing
-dnf5 -y copr disable biezczaders/kernel-cachyos-addons
-
+sudo systemctl enable --now uksmd.service
+dnf5 -y copr disable bieszczaders/kernel-cachyos-addons 
 echo 'Cachy OS Kernel Installed with Addons'
 
 #Install MUTTER PERFORMANCE 'BUILD'
