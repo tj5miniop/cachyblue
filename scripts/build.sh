d506a3146a945d00e48843d7630c49bc38ab81a4
tj5miniop <82961157+tj5miniop@users.noreply.github.com>
2025-02-16 21:31:13 +0000

    Update build.sh - add allowerasing flag to certain commands

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,15 +11,15 @@ echo 'Updated!'
 #Install CachyOS kernel 
 dnf5 -y copr enable bieszczaders/kernel-cachyos
 dnf5 -y update --refresh 
-dnf5 -y remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
-dnf5 -y install kernel-cachyos
+dnf5 -y remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra --allowerasing
+dnf5 -y install kernel-cachyos --allowerasing
 
 echo 'CachyOS Kernel Installed'
 
 #Add extra stuff for performance 
 dnf5 -y copr disable bieszczaders/kernel-cachyos
 dnf5 -y copr enable bieszczaders/kernel-cachyos-addons 
-dnf5 -y install cachyos-settings cachyos-kcm-settings uksmd cachyos-ananicy-rules ananicy-cpp
+dnf5 -y install cachyos-settings cachyos-kcm-settings uksmd cachyos-ananicy-rules ananicy-cpp --allowerasing
 dnf5 -y copr disable biezczaders/kernel-cachyos-addons
 
 echo 'Cachy OS Kernel Installed with Addons'
