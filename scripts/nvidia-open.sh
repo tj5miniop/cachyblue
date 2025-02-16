e85e538fe1cf67fc6f4ee8b8473f582b58e01bd1
tj5miniop <82961157+tj5miniop@users.noreply.github.com>
2025-02-16 23:28:51 +0000

    Update nvidia-open.sh
    Hopefully fix nvidia issues.

--- scripts/nvidia-open.sh
+++ scripts/nvidia-open.sh
@@ -1,6 +1,7 @@
-
-
+#Add RPM Fusion repos
+dnf5 -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf5 -y update --refresh 
 #This Updates the NVIDIA Drivers to the latest 'open' kernel module variant
 
 dnf5 -y install rpmfusion-nonfree-release-tainted 
-dnf5 -y swap akmod-nvidia akmod-nvidia-open
+dnf5 -y install akmod-nvidia-open
