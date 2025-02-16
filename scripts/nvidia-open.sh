bd6a54ecc84ef263c82a9c8949e6b33a9e1123c7
tj5miniop <82961157+tj5miniop@users.noreply.github.com>
2025-02-16 21:29:08 +0000

    Update nvidia-open.sh

---
+++ scripts/nvidia-open.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#This Updates the NVIDIA Drivers to the latest 'open' kernel module variant
+
+dnf5 -y install rpmfusion-nonfree-release-tainted 
+dnf5 -y swap akmod-nvidia akmod-nvidia-open
