58d959f6c5f30a666e937193d9d60e36ae81ed99
tj5miniop <82961157+tj5miniop@users.noreply.github.com>
2025-02-16 21:24:57 +0000

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,7 +10,7 @@ echo 'Updated!'
 
 #Install CachyOS kernel 
 dnf5 -y copr enable bieszczaders/kernel-cachyos
-dnf5 -y update
+dnf5 -y update --refresh 
 dnf5 -y remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
 dnf5 -y install kernel-cachyos
 
@@ -18,13 +18,14 @@ echo 'CachyOS Kernel Installed'
 
 #Add extra stuff for performance 
 dnf5 -y copr disable bieszczaders/kernel-cachyos
-dnf5 -y install cachyos-settings cachyos-kcm-settings uksmd cachyos-ananicy-rules ananicy-cpp
 dnf5 -y copr enable bieszczaders/kernel-cachyos-addons 
+dnf5 -y install cachyos-settings cachyos-kcm-settings uksmd cachyos-ananicy-rules ananicy-cpp
+dnf5 -y copr disable biezczaders/kernel-cachyos-addons
 
 echo 'Cachy OS Kernel Installed with Addons'
 
 #Install MUTTER PERFORMANCE 'BUILD'
-dnf -y copr enable execat/mutter-performance 
-dnf5 -y update
-
+dnf5 -y copr enable execat/mutter-performance 
+dnf5 -y update --refresh
+dnf5 -y copr disable execat/mutter-performance 
 echo 'Done'
