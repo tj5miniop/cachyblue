7331638265cbde930f199f2bf7eab8464071a002
tj5miniop <82961157+tj5miniop@users.noreply.github.com>
2025-02-16 23:38:50 +0000

    Update nvidia-open.sh
    Specify RPM fusion

--- scripts/nvidia-open.sh
+++ scripts/nvidia-open.sh
@@ -1,5 +1,5 @@
 #Add RPM Fusion repos
-dnf5 -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf5 -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-41.noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-41.noarch.rpm
 dnf5 -y update --refresh 
 #This Updates the NVIDIA Drivers to the latest 'open' kernel module variant
 
