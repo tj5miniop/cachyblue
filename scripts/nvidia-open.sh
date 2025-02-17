e6b57a53e980ee7c662ce7daeeca02a71e710eb1
tj5miniop <82961157+tj5miniop@users.noreply.github.com>
2025-02-17 00:16:02 +0000

    Update nvidia-open.sh

--- scripts/nvidia-open.sh
+++ scripts/nvidia-open.sh
@@ -2,5 +2,5 @@
 dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 dnf -y update --refresh 
 #This Updates the NVIDIA Drivers to the latest 'open' kernel module variant
- dnf -y install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-tainted-$(rpm -E %fedora).noarch.rpm
+dnf -y install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-tainted-$(rpm -E %fedora).noarch.rpm
 dnf -y install akmod-nvidia-open
